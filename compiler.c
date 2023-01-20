df523574a9d3dd884438efb2e5de24f004a79590
Ross Morrison <rossmorrison53@yahoo.com>
2023-01-19 20:39:29 -0600

    17.6 a pratt parser/17.7 dumping chunks

--- compiler.c
+++ compiler.c
@@ -3,6 +3,9 @@
 #include "compiler.h"
 #include "scanner.h"
 #include <stdlib.h>
+#ifdef DEBUG_PRINT_CODE
+#include "debug.h"
+#endif
 
 typedef struct
 {
@@ -27,6 +30,8 @@ typedef enum
 	PREC_PRIMARY
 } Precedence;	
 
+typedef void (*ParseFn)();
+
 typedef struct {
 	ParseFn prefix;
 	ParseFn infix;
@@ -36,9 +41,26 @@ typedef struct {
 Parser parser;
 Chunk* compilingChunk;
 
+static ParseRule* getRule(TokenType type);
+
 static void parsePrecedence(Precedence precendence)
 {
-	
+	advance();
+	ParseFn prefixRule = getRule(parser.previous.type)->prefix;
+	if (prefixRule == NULL)
+	{
+		error("Expect expression.");
+		return;
+	}	
+
+	prefixRule();
+
+	while (precedence <= getRule(parser.current.type)->precedence)
+	{
+		advance();
+		ParseFn infixRule = getRule(parser.previous.type)->infix;
+		infixRule();
+	}
 }
 
 static Chunk* currentChunk()
@@ -139,6 +161,12 @@ static void emitConstant(Value value)
 static void endCompiler()
 {
 	emitReturn();
+#ifdef DEBUG_PRINT_CODE
+	if (!parser.hadError)
+	{
+		disassembleChunk(currentChunk(), "code");
+	}
+#endif
 }
 
 static void expression()
@@ -182,11 +210,59 @@ static void binary()
 	{
 		case TOKEN_PLUS: 	emitByte(OP_ADD); break;
 		case TOKEN_MINUS:	emitByte(OP_SUBTRACT); break;
-		case TOKEN_STAR:	emiteByte(OP_MULTIPLY); break;
+		case TOKEN_STAR:	emitByte(OP_MULTIPLY); break;
 		case TOKEN_SLASH:	emitByte(OP_DIVIDE); break;
 		default: 
 			return;
 	}
+}
+
+ParseRule rules[] = {
+	[TOKEN_LEFT_PAREN]    = {grouping, NULL, PREC_NONE},
+	[TOKEN_RIGHT_PAREN]   = {NULL, NULL, PREC_NONE},
+	[TOKEN_LEFT_BRACE]    = {NULL, NULL, PREC_NONE},
+	[TOKEN_RIGHT_BRACE]   = {NULL, NULL, PREC_NONE},
+	[TOKEN_COMMA]	      = {NULL, NULL, PREC_NONE},
+	[TOKEN_DOT] 	      = {NULL, NULL, PREC_NONE},
+	[TOKEN_MINUS]         = {unary, binary, PREC_TERM},
+	[TOKEN_PLUS] 	      = {NULL, binary, PREC_TERM},
+	[TOKEN_SEMICOLON]     = {NULL, NULL, PREC_NONE},
+	[TOKEN_SLASH]	      = {NULL, binary, PREC_FACTOR},
+	[TOKEN_STAR]   	      = {NULL, binary, PREC_FACTOR},
+	[TOKEN_BANG]          = {NULL, NULL, PREC_NONE},
+	[TOKEN_BANG_EQUAL]    = {NULL, NULL, PREC_NONE},
+	[TOKEN_EQUAL]         = {NULL, NULL, PREC_NONE},
+	[TOKEN_EQUAL_EQUAL]   = {NULL, NULL, PREC_NONE},
+	[TOKEN_GREATER]       = {NULL, NULL, PREC_NONE},
+	[TOKEN_GREATER_EQUAL] = {NULL, NULL, PREC_NONE},
+        [TOKEN_LESS]          = {NULL, NULL, PREC_NONE},
+        [TOKEN_LESS_EQUAL]    = {NULL, NULL, PREC_NONE},
+        [TOKEN_IDENTIFIER]    = {NULL, NULL, PREC_NONE},
+        [TOKEN_STRING]        = {NULL, NULL, PREC_NONE},
+	[TOKEN_NUMBER]	      = {number, NULL, PREC_NONE},
+	[TOKEN_AND]	      = {NULL, NULL, PREC_NONE},
+	[TOKEN_CLASS]	      = {NULL, NULL, PREC_NONE},
+	[TOKEN_ELSE]	      = {NULL, NULL, PREC_NONE},
+	[TOKEN_FALSE]	      = {NULL, NULL, PREC_NONE},
+	[TOKEN_FOR]	      = {NULL, NULL, PREC_NONE},
+	[TOKEN_FUN]	      = {NULL, NULL, PREC_NONE},
+	[TOKEN_IF]  	      = {NULL, NULL, PREC_NONE},
+	[TOKEN_NIL]	      = {NULL, NULL, PREC_NONE},
+	[TOKEN_OR]	      = {NULL, NULL, PREC_NONE},
+	[TOKEN_PRINT]         = {NULL, NULL, PREC_NONE},
+	[TOKEN_RETURN]        = {NULL, NULL, PREC_NONE},
+	[TOKEN_SUPER]	      = {NULL, NULL, PREC_NONE},
+	[TOKEN_THIS]	      = {NULL, NULL, PREC_NONE},
+	[TOKEN_TRUE]	      = {NULL, NULL, PREC_NONE},
+	[TOKEN_VAR]	      = {NULL, NULL, PREC_NONE},
+	[TOKEN_WHILE]         = {NULL, NULL, PREC_NONE},
+	[TOKEN_ERROR]	      = {NULL, NULL, PREC_NONE},
+	[TOKEN_EOF]	      = {NULL, NULL, PREC_NONE},	
+};
+
+static ParseRule* getRule(TokenType type) 
+{
+	return &rules[type];
 }
 
 bool compile(const char* source, Chunk* chunk)
