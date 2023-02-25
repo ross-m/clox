d382514e264a28deaa8530e88b15770642f43fbf
Ross Morrison <rossmorrison53@yahoo.com>
2023-02-24 23:32:25 -0600

    19.5 freeing objects

--- object.c
+++ object.c
@@ -13,6 +13,9 @@ static Obj* allocateObject(size_t size, ObjType type)
 {
 	Obj* object = (Obj*)reallocate(NULL, 0, size);
 	object->type = type;
+
+	object->next = vm.objects;
+	vm.objects = object;
 	return object;
 }
 
@@ -24,10 +27,25 @@ static ObjString* allocateString(char* chars, int length)
 	return string;
 }
 
+ObjString* takeString(char* chars, int length)
+{
+	return allocateString(chars, length);
+}
+
 ObjString* copyString(const char* chars, int length)
 {
 	char* heapChars = ALLOCATE(char, length + 1);
 	memcpy(heapChars, chars, length);
 	heapChars[length] = '\0';
 	return allocateString(heapChars, length);
+}
+
+void printObject(Value value)
+{
+	switch (OBJ_TYPE(value))
+	{
+		case OBJ_STRING:
+			printf("%s", AS_CSTRING(value));
+			break;
+	}
 }
--- object.h
+++ object.h
@@ -19,6 +19,7 @@ typedef enum
 struct Obj
 {
 	ObjType type;
+	struct Obj* next;
 };
 
 struct ObjString
@@ -28,7 +29,9 @@ struct ObjString
 	char* chars;
 };
 
+ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char* chars, int length);
+void printObject(Value value);
 
 static inline bool isObjType(Value value, ObjType type)
 {
